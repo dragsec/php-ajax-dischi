6994d9aa7bfa3df9f58a53fb72213970d37f9f88
dragsec <draganyes@gmail.com>
2021-06-03 18:06:03 +0200

    finito

--- database.php
+++ database.php
@@ -3,19 +3,27 @@
 $discs =  [
 
     [
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/61%2BWf3mHoOL._AC_SL1200_.jpg",
+        "author" => "Blackpink",
+        "title" => "The Album",
+        "year" => "2020",
+        "genre" => "K-pop",
+    ],
+
+    [
         "image" => "https://images-na.ssl-images-amazon.com/images/I/81hryXAVZjL._AC_SL1425_.jpg",
         "author" => "Metallica",
         "title" => "Master of Puppets",
         "year" => "1986",
         "genre" => "Metal",
     ],
-
+    
     [
-        "image" => "https://images-na.ssl-images-amazon.com/images/I/61%2BWf3mHoOL._AC_SL1200_.jpg",
-        "author" => "Blackpink",
-        "title" => "The Album",
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/A1vy4PUuSCL._AC_SL1500_.jpg",
+        "author" => "Grimes",
+        "title" => "Miss Anthropocene",
         "year" => "2020",
-        "genre" => "K-pop",
+        "genre" => "Alternative",
     ],
 
     [
@@ -43,19 +51,35 @@ $discs =  [
     ],
 
     [
-        "image" => "https://images-na.ssl-images-amazon.com/images/I/A1vy4PUuSCL._AC_SL1500_.jpg",
-        "author" => "Grimes",
-        "title" => "Miss Anthropocene",
-        "year" => "2020",
-        "genre" => "Alternative",
-    ],
-
-    [
         "image" => "https://upload.wikimedia.org/wikipedia/en/9/91/Marilyn_Manson_-_Holy_Wood.png",
         "author" => "Marilyn Manson",
         "title" => "Holy Wood",
         "year" => "2000",
         "genre" => "Industrial",
+    ],
+
+    [
+        "image" => "https://upload.wikimedia.org/wikipedia/en/b/b6/Dynasty_%28album%29_cover.jpg",
+        "author" => "Kiss",
+        "title" => "Dynasty",
+        "year" => "1979",
+        "genre" => "Rock",
+    ],
+
+    [
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/71RhxjTkcJL._AC_SL1232_.jpg",
+        "author" => "Skrillex",
+        "title" => "Scary Monsters and Nice Sprites",
+        "year" => "2010",
+        "genre" => "Electronic",
+    ],
+
+    [
+        "image" => "https://upload.wikimedia.org/wikipedia/en/8/83/Masters_of_Chant_II.jpg",
+        "author" => "Gregorian",
+        "title" => "Masters of Chant Chapter II",
+        "year" => "2001",
+        "genre" => "Gregorian Chant",
     ],
 
     
