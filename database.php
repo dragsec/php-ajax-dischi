c55b5a957df289fb834d5203d93bc5d5492fc030
dragsec <draganyes@gmail.com>
2021-06-03 17:33:48 +0200

    test2

--- database.php
+++ database.php
@@ -3,43 +3,43 @@
 $discs =  [
 
     [
-        "image" => "",
-        "author" => "",
-        "title" => "",
-        "year" => "",
-        "genre" => "",
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/81hryXAVZjL._AC_SL1425_.jpg",
+        "author" => "Metallica",
+        "title" => "Master of Puppets",
+        "year" => "1986",
+        "genre" => "Metal",
     ],
 
     [
-        "image" => "",
-        "author" => "",
-        "title" => "",
-        "year" => "",
-        "genre" => "",
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/61%2BWf3mHoOL._AC_SL1200_.jpg",
+        "author" => "Blackpink",
+        "title" => "The Album",
+        "year" => "2020",
+        "genre" => "K-Pop",
     ],
 
     [
-        "image" => "",
-        "author" => "",
-        "title" => "",
-        "year" => "",
-        "genre" => "",
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/817nxzXbEYL._AC_SL1200_.jpg",
+        "author" => "6ix9ine",
+        "title" => "Day69 Graduation Day",
+        "year" => "2018",
+        "genre" => "Rap",
     ],
 
     [
-        "image" => "",
-        "author" => "",
-        "title" => "",
-        "year" => "",
-        "genre" => "",
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/71BgJkNpWbL._AC_SL1417_.jpg",
+        "author" => "Melanie Martinez",
+        "title" => "Cry Baby",
+        "year" => "2015",
+        "genre" => "Pop",
     ],
 
     [
-        "image" => "",
-        "author" => "",
-        "title" => "",
-        "year" => "",
-        "genre" => "",
+        "image" => "https://images-na.ssl-images-amazon.com/images/I/41jNiCE9%2B8L._AC_.jpg",
+        "author" => "Goblin",
+        "title" => "Profondo Rosso",
+        "year" => "1975",
+        "genre" => "Soundtrack",
     ],
 
     
